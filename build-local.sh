8d3ebde89ff11a7ce92765fca281c300d9092b39
Pontus <contrib@pontusnetworks.com>
2018-08-25 23:39:14 +0100

    almost working, but not quite; trying back with knox

--- build-local.sh
+++ build-local.sh
@@ -11,11 +11,15 @@ cd $DIR
 echo DIR is $DIR
 echo TARFILE is $TARFILE
 
+if [[ ! -d $DISTDIR ]]; then
+  mkdir -p $DISTDIR;
+fi
+
 if [[ ! -f $TARFILE ]]; then
 
-yum -y install attr bind-utils docbook-style-xsl gcc gdb krb5-workstation        libsemanage-python libxslt perl perl-ExtUtils-MakeMaker        perl-Parse-Yapp perl-Test-Base pkgconfig policycoreutils-python        python-crypto gnutls-devel libattr-devel keyutils-libs-devel        libacl-devel libaio-devel libblkid-devel libxml2-devel openldap-devel        pam-devel popt-devel python-devel readline-devel zlib-devel systemd-devel
+yum -y install attr bind-utils docbook-style-xsl gcc gdb krb5-workstation        libsemanage-python libxslt perl perl-ExtUtils-MakeMaker        perl-Parse-Yapp perl-Test-Base pkgconfig policycoreutils-python        python-crypto gnutls-devel libattr-devel keyutils-libs-devel        libacl-devel libaio-devel libblkid-devel libxml2-devel openldap-devel        pam-devel popt-devel python-devel readline-devel zlib-devel systemd-devel openssl-devel
 
-./auto/configure  --prefix=/opt/pontus/pontus-nginx/nginx-${VERSION}
+./auto/configure  --prefix=/opt/pontus/pontus-nginx/nginx-${VERSION} --with-http_ssl_module --with-threads  --with-poll_module --with-http_gzip_static_module --with-http_secure_link_module  --with-http_sub_module --with-http_v2_module --user=pontus
 make -j 4
 make install
 
@@ -39,19 +43,35 @@ cd current
 cat <<"EOF" >> config-nginx.sh
 #!/bin/bash
 
-export VERSION=$VERSION
-if [[ -f /opt/pontus/pontus-nginx/nginx-${VERSION}/conf/nginx.conf ]]; then
-  mv /opt/pontus/pontus-nginx/nginx-${VERSION}/conf/nginx.conf  /opt/pontus/pontus-nginx/nginx-${VERSION}/conf/nginx.conf.orig
+if [[ -f /opt/pontus/pontus-nginx/current/conf/nginx.conf ]]; then
+  mv /opt/pontus/pontus-nginx/current/conf/nginx.conf  /opt/pontus/pontus-nginx/current/conf/nginx.conf.orig
 fi
 
-> /opt/pontus/pontus-nginx/nginx-${VERSION}/conf/nginx.conf
 
-cat << 'EOF2' >> /opt/pontus/pontus-nginx/nginx-${VERSION}/conf/nginx.conf
+> /opt/pontus/pontus-nginx/current/conf/proxy.conf
+cat << 'EOF2' >> /opt/pontus/pontus-nginx/current/conf/proxy.conf
+proxy_redirect          off;
+proxy_set_header        Host            $host;
+proxy_set_header        X-Real-IP       $remote_addr;
+proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
+client_max_body_size    10m;
+client_body_buffer_size 128k;
+proxy_connect_timeout   90;
+proxy_send_timeout      90;
+proxy_read_timeout      90;
+proxy_buffers           32 4k;
+
+EOF2
+
+
+> /opt/pontus/pontus-nginx/current/conf/nginx.conf
+
+cat << 'EOF2' >> /opt/pontus/pontus-nginx/current/conf/nginx.conf
 user  pontus;
 worker_processes  1;
 
-error_log  /var/log/nginx/error.log warn;
-pid        /var/run/nginx.pid;
+error_log  /opt/pontus/pontus-nginx/current/logs/error.log debug;
+pid        /opt/pontus/pontus-nginx/current/nginx.pid;
 
 
 
@@ -63,13 +83,14 @@ events {
 
 http {
     include       /etc/nginx/mime.types;
+    include       /etc/nginx/proxy.conf;
     default_type  application/octet-stream;
 
     log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
                       '\$status \$body_bytes_sent "\$http_referer" '
                       '"\$http_user_agent" "\$http_x_forwarded_for"';
 
-    access_log  /var/log/nginx/access.log  main;
+    access_log  /opt/pontus/pontus-nginx/current/logs/access.log  main;
 
     sendfile        on;
     #tcp_nopush     on;
@@ -80,8 +101,12 @@ http {
 
     include /opt/pontus/pontus-nginx/conf/conf.d/*.conf;
 
+    upstream pvgdprgui {
+      server 127.0.0.1:3000 weight=3;
+    }
+
     server {
-        root /wwwroot;
+        root /;
         ssl_protocols               TLSv1 TLSv1.1 TLSv1.2 TLSv1.3;
         ssl_ciphers                 ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256;
         ssl_prefer_server_ciphers   on;
@@ -90,27 +115,100 @@ http {
         listen       8443 ssl;
         server_name  pontus-sandbox.pontusvision.com;
 
-        ssl_certificate      /etc/pki/private/localhost.pem;
-        ssl_certificate_key  /etc/pki/private/localhost.crt;
+        ssl_certificate      /etc/pki/private/localhost.crt;
+        ssl_certificate_key  /etc/pki/private/localhost.pem;
 
         ssl_session_cache    shared:SSL:1m;
         ssl_session_timeout  5m;
 
-        ssl_ciphers  HIGH:!aNULL:!MD5;
-        ssl_prefer_server_ciphers  on;
+        #ssl_ciphers  HIGH:!aNULL:!MD5;
 
-        location / {
-          root   html;
-          index  index.html index.htm;
+        location ~ ^/auth.* {
+           rewrite ^(/auth/.*) $1 break;
+           proxy_pass      https://localhost:5005;
+           proxy_set_header Host              $host;
+           proxy_set_header X-Real-IP         $remote_addr;
+           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+           proxy_set_header X-Forwarded-Proto https;
+
+           #proxy_set_header    Upgrade $http_upgrade;
+           #proxy_set_header    Connection "upgrade";
+           #proxy_set_header    Host $host;
+           proxy_set_header    X-NginX-Proxy true;
+
+           proxy_http_version  1.1;
+           proxy_redirect      off;
+
+
+        }
+
+        location ~ ^/nifi.* {
+           rewrite ^(/nifi.*) $1 break;
+           proxy_pass      http://127.0.0.1:5007;
+        }
+
+
+        location ~ ^/gateway/sandbox/pvgdpr_server.* {
+           rewrite ^/gateway/sandbox/pvgdpr_server/(.*)$ /$1 break;
+           proxy_pass      http://127.0.0.1:3001;
+        }
+
+        location ~ ^/gateway/sandbox/pvgdpr_graph.*  {
+           rewrite ^/gateway/sandbox/pvgdpr_graph/(.*)$ /$1 break;
+           proxy_pass      http://127.0.0.1:8182;
+        }
+
+
+        #location ~  ^/full.* {
+        #   rewrite_log on;
+        #   rewrite ^/full/(.*) /pvgdpr/$1 break;
+#
+#           proxy_pass      http://127.0.0.1:3000;
+#
+#           #sub_filter_types text/html text/css text/xml;
+#           #sub_filter /pvgdpr/pvgdpr /pvgdpr;
+#
+#           proxy_set_header Host $host;
+#           proxy_cache_bypass true;
+#           proxy_no_cache true;
+#           proxy_set_header X-Real-IP $remote_addr;
+#           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+#
+#          }
+
+        location ~ ^/pvgdpr.* {
+           rewrite_log on;
+           rewrite ^/pvgdpr/pvgdpr(/.*) $1 break;
+           rewrite ^/pvgdpr/pvgdpr_gui(/.*) $1 break;
+           rewrite ^/pvgdpr/../static/(.*) /static/$1 break;
+           rewrite ^/pvgdpr/full/(.*) /$1 break;
+           rewrite ^/pvgdpr/full(.*)  /$1 break;
+           rewrite ^/pvgdpr/expert/(.*) /$1 break;
+           rewrite ^/pvgdpr/expert(.*) /$1 break;
+           rewrite ^/pvgdpr/re/(.*) /$1 break;
+           rewrite ^/pvgdpr/re(.*) /$1 break;
+           rewrite ^/pvgdpr(/.*) $1 break;
+
+           proxy_pass      http://127.0.0.1:3000;
+
+           #sub_filter_types text/html text/css text/xml;
+           #sub_filter /pvgdpr/pvgdpr /pvgdpr;
+
+           proxy_set_header Host $host;
+           proxy_cache_bypass true;
+           proxy_no_cache true;
+           proxy_set_header X-Real-IP $remote_addr;
+           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+
+
         }
 
         location / {
-            root /wwwroot;
-            index index.html;
-
-            try_files $uri $uri/ /wwwroot/index.html;
+            # First attempt to serve request as file, then
+            # as directory, then fall back to displaying a 404.
+            #try_files $uri $uri/ /index.html /index.js;
+            try_files $uri $uri/ /index.html ;
         }
-
 
     }
 }
@@ -134,6 +232,7 @@ WantedBy=multi-user.target
 
 EOF2
 
+ln -s /opt/pontus/pontus-nginx/current/conf /etc/nginx
 chown -R pontus: /opt/pontus/pontus-nginx
 systemctl enable pontus-nginx
 systemctl daemon-reload
